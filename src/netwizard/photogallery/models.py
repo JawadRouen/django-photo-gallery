f3ced7dbce0c8328a776e7485b4e796a3cb95d91
Marcin Nowak <marcin.j.nowak@gmail.com>
2009-04-18 16:20:51 +0200

    initial revision

---
+++ src/netwizard/photogallery/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+import os
+
+class Photo(models.Model):
+    image = models.ImageField(
+            max_length=255,
+            upload_to=os.path.join('uploads','photogallery')
+            )
+    title = models.CharField(max_length=255, null=True, blank=True)
+    description = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
+
