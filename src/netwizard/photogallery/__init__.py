cef3568503d0cba9e06ee9ce3b5ff653c99001c0
Marcin Nowak <marcin@dracoarc.localdomain>
2009-07-07 01:43:32 +0200

    sitemenu support

--- src/netwizard/photogallery/__init__.py
+++ src/netwizard/photogallery/__init__.py
@@ -1,1 +1,8 @@
 import plugins
+
+try:
+    from netwizard.antyadmin import sitemenu
+    from django.conf import settings
+    sitemenu.connect('apps', 'Photo gallery', settings.ADMIN_URL+'photogallery', pri=5, name='apps.photogallery')
+except ImportError:
+    pass
